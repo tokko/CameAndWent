3d146435beb8900a3904f84f5e18c18fb5e3d157
tokko <andreg@kth.se>
2015-02-03 20:15:45 +0100

    notification now opens loglist

--- app/src/main/java/com/tokko/cameandwent/cameandwent/ClockManager.java
+++ app/src/main/java/com/tokko/cameandwent/cameandwent/ClockManager.java
@@ -2,8 +2,10 @@ package com.tokko.cameandwent.cameandwent;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.media.RingtoneManager;
@@ -62,6 +64,7 @@ public class ClockManager {
         nb.setContentTitle(s);
         nb.setSmallIcon(R.drawable.ic_launcher);
         nb.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+        nb.setContentIntent(PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK), PendingIntent.FLAG_UPDATE_CURRENT));
         NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         nm.notify(id, nb.build());
     }
