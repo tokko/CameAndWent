1f61034582fa29ef1f22ea460c76d2e14756962a
tokko <andreg@kth.se>
2015-03-20 21:17:15 +0100

    integrated tag management into preferences

--- app/src/main/java/com/tokko/cameandwent/cameandwent/LocationPreference.java
+++ app/src/main/java/com/tokko/cameandwent/cameandwent/LocationPreference.java
@@ -1,62 +1,28 @@
 package com.tokko.cameandwent.cameandwent;
 
 import android.content.Context;
-import android.location.Location;
-import android.os.Bundle;
+import android.content.Intent;
 import android.preference.Preference;
 import android.util.AttributeSet;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
+import com.tokko.cameandwent.cameandwent.locationtags.LocationTagActivity;
 
-public class LocationPreference extends Preference implements Preference.OnPreferenceClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class LocationPreference extends Preference implements Preference.OnPreferenceClickListener {
 
     private GoogleApiClient mGoogleApiClient;
+    private Context context;
 
     public LocationPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.context = context;
         setOnPreferenceClickListener(this);
     }
 
     @Override
     public boolean onPreferenceClick(Preference preference) {
-        buildGoogleApiClient();
-        mGoogleApiClient.connect();
+        context.startActivity(new Intent(context, LocationTagActivity.class));
         return true;
     }
 
-    protected synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(getContext())
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                mGoogleApiClient);
-        if (mLastLocation != null) {
-            Toast.makeText(getContext(), String.format("Longitude: %f\nLatitude %f", mLastLocation.getLongitude(), mLastLocation.getLatitude()), Toast.LENGTH_SHORT).show();;
-            persistString(mLastLocation.getLatitude() + ";" + mLastLocation.getLongitude());
-        }
-        else
-            Toast.makeText(getContext(), "Location is null", Toast.LENGTH_SHORT).show();
-
-
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-
-    }
 }
