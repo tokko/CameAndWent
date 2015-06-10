c8ab98a969d2456f1259972654bef6cfeff66674
tokko <andreg@kth.se>
2015-06-10 20:24:50 +0200

    delayed alarm is scheduled properly

--- app/src/test/java/com/tokko/cameandwent/cameandwent/GeofenceServiceTests.java
+++ app/src/test/java/com/tokko/cameandwent/cameandwent/GeofenceServiceTests.java
@@ -1,5 +1,8 @@
 package com.tokko.cameandwent.cameandwent;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 
@@ -10,15 +13,26 @@ import com.google.inject.Binder;
 import com.google.inject.Module;
 import com.tokko.cameandwent.cameandwent.clockmanager.ClockManager;
 import com.tokko.cameandwent.cameandwent.services.GeofenceService;
+import com.tokko.cameandwent.cameandwent.util.TimeConverter;
 
+import junit.framework.Assert;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeConstants;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
+import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowAlarmManager;
+import org.robolectric.shadows.ShadowPendingIntent;
 import org.robolectric.shadows.ShadowPreferenceManager;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import roboguice.RoboGuice;
 
@@ -65,8 +79,17 @@ public class GeofenceServiceTests {
     @Test
     public void delayedClockout_AlarmIsScheduled(){
         sharedPreferences.edit().putBoolean("delayed_clockout", true).apply();
+        TimeConverter.CURRENT_TIME = new DateTime(2010, 3, 14, 13, 45, 1).getMillis();
         service.onHandleIntent(exitIntent);
         verify(mockClockManager, never()).clockOut();
+
+        AlarmManager alarmManager = (AlarmManager) RuntimeEnvironment.application.getSystemService(Context.ALARM_SERVICE);
+        ShadowAlarmManager shadowAlarmManager = Shadows.shadowOf(alarmManager);
+        ShadowAlarmManager.ScheduledAlarm nextAlarm = shadowAlarmManager.getScheduledAlarms().get(0);
+        Assert.assertEquals(TimeConverter.CURRENT_TIME + 5* DateTimeConstants.MILLIS_PER_MINUTE, nextAlarm.triggerAtTime);
+        Assert.assertEquals(0, nextAlarm.interval);
+        ShadowPendingIntent spi = Shadows.shadowOf(nextAlarm.operation);
+        Assert.assertEquals(GeofenceService.DELAYED_DEACTIVATION, spi.getSavedIntent().getAction());
     }
 
     private class GeofenceServiceMock extends GeofenceService{
